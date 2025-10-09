03c981b480e5c91c1859b4472ae30ebd356d12af
Emilie M. <emilie.marion.simplon@gmail.com>
2025-10-09 17:01:51 +0200

    added productRoute to main router

--- backend/src/Routes/index.ts
+++ backend/src/Routes/index.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import testRouter from "./testRouter";
-
+import productRouter from "./productRouter";
 
 const router = Router();
 
 router.use("/test", testRouter);
 
+router.use("/products", productRouter);
 
 export default router;
