b25ed8fa241b973e552f5bd6f1c8a7a6e40fb7d6
Dim <Dimitri86120@outlook.com>
2025-10-16 13:45:26 +0200

    mise au propre en groupe

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,21 +1,22 @@
+import FormRegisterpage from "./components/pages/FomRegisterPage";
+import FormLoginPage from "./components/pages/FormLoginPage";
 import CategoryPage from "./components/pages/CategoryPage";
 import AllProductPage from "./components/pages/Allproduct";
 import ProductPage from "./components/pages/ProductPage";
-import LoginForm from "./components/forms/loginForms";
 import Homepage from "./components/pages/HomePage";
 import { Route, Routes } from "react-router";
-import Form from "./components/forms/forms";
+
 
 function Router() {
 
   return (
     <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/categories" element={<CategoryPage />} />
-        <Route path="/products" element={<AllProductPage />} />
+        <Route path="/register" element={<FormRegisterpage />} />
         <Route path="/products/:id" element={<ProductPage />} />
-        <Route path="/register" element={<Form />} />
-        <Route path="/login" element={<LoginForm />} />
+        <Route path="/products" element={<AllProductPage />} />
+        <Route path="/categories" element={<CategoryPage />} />
+        <Route path="/login" element={<FormLoginPage />} />
+        <Route path="/" element={<Homepage />} />
         {/*<Route path="/cart" element={"bite"} />
         <Route path="/payment" element={"bite"} />
         <Route path="/history" element={"bite"} />  */}
