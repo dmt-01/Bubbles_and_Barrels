7a349b1f3f5f48608601340b81594137432da1b6
fouzy213 <fouzy_69@hotmail.fr>
2025-10-17 16:48:29 +0200

    css_homepage

--- frontend/src/components/Sale/sale.tsx
+++ frontend/src/components/Sale/sale.tsx
@@ -21,17 +21,22 @@ function Sale() {
   }, []);
 
   return (
-    <div>
+    <div className="all_sale">
       <h2>Liste des promotions</h2>
       <ul>
         {sales.map(({ product, sale, discountedPrice }) => (
+          <div>
           <li key={sale.sale_id}>
-            <img src={product.image} alt={product.name} />
+            <img
+        src={`http://localhost:5000/image/${product.image}`}
+        alt={product.name}
+      />
             <p>Produit : {product.name}</p>
             <p>Prix original : {product.price} €</p>
             <p>Réduction : {sale.discount}%</p>
             <p>Prix après remise : {discountedPrice} €</p>
           </li>
+          </div>
         ))}
       </ul>
     </div>
